110c37d94591ed45656b0c9365096c33b8dc1176
Deamons <truc.bugcreator666@gmail.com>
2021-04-09 08:11:06 +0700

    update style & scripts

--- src/app/components/home-user/home-user.component.ts
+++ src/app/components/home-user/home-user.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-home-user',
   templateUrl: './home-user.component.html',
   styleUrls: ['./home-user.component.css']
 })
-export class HomeUserComponent implements OnInit {
+
+export class HomeUserComponent implements OnInit, AfterViewInit {
 
   constructor() { }
+  ngAfterViewInit(): void {
+    $("#readmore").on("click", function() {
+      let dots = $("#dots");
+      let moreText =$("#readmore-content"); document.getElementById("readmore-content");
+      let btnText =$("#readmore");
+
+      if (dots.css("display") === "none") {
+          dots.css("display","inline");
+          btnText.text("Xem thêm");
+          moreText.css("display","none");
+      } else {
+          dots.css("display","none");
+          btnText.text("Thu gọn");
+          moreText.css("display","inline");
+      }
+  });
+  }
 
   ngOnInit(): void {
   }
+
+  
 
 }
