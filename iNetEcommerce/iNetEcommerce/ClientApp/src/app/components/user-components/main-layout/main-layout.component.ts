12bb75d6748057ee40aa8ffa8d476855ad76c507
Deamons <truc.bugcreator666@gmail.com>
2021-04-11 14:16:43 +0700

    update menu responsive for text size

--- iNetEcommerce/iNetEcommerce/ClientApp/src/app/components/user-components/main-layout/main-layout.component.ts
+++ iNetEcommerce/iNetEcommerce/ClientApp/src/app/components/user-components/main-layout/main-layout.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.css']
 })
-export class MainLayoutComponent implements OnInit {
+export class MainLayoutComponent implements OnInit,AfterViewInit {
 
   constructor() { }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
+    $("#menu-icon-left").on("click", function() {
+      let $nav = $(".navbar-custom");
+      if ($nav.css("display") === "none") {
+        $nav.attr('style', 'display:block !important');
+    } else {
+      $nav.attr('style', 'display:none !important');
+    }
+  });
   }
 
 }
