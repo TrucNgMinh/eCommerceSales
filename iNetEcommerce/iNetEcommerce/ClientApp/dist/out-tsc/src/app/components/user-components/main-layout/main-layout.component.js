e597a7e34e340101027ec790bb884df4e0581572
haonguyen <nhenden1511@gmail.com>
2021-04-11 16:47:32 +0700

    update xml

--- iNetEcommerce/iNetEcommerce/ClientApp/dist/out-tsc/src/app/components/user-components/main-layout/main-layout.component.js
+++ iNetEcommerce/iNetEcommerce/ClientApp/dist/out-tsc/src/app/components/user-components/main-layout/main-layout.component.js
@@ -2,7 +2,22 @@ import { __decorate } from "tslib";
 import { Component } from '@angular/core';
 let MainLayoutComponent = class MainLayoutComponent {
     constructor() { }
+    ngAfterViewInit() {
+        throw new Error('Method not implemented.');
+    }
     ngOnInit() {
+        $("#menu-icon-left").on("click", function () {
+            let $navCustom = $(".navbar-custom");
+            let $nav = $("#navigation");
+            if ($navCustom.css("display") === "none") {
+                $navCustom.attr('style', 'display:block !important');
+                $nav.attr('style', 'display:block !important');
+            }
+            else {
+                $navCustom.attr('style', 'display:none !important');
+                $nav.attr('style', 'display:none !important');
+            }
+        });
     }
 };
 MainLayoutComponent = __decorate([
