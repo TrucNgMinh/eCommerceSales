7d20cf93802eb25e3ca58315e0ad79675b870d8b
TrucNguyen <truc.nguyen@cloudconnecttech.com>
2021-05-16 22:57:48 +0700

    set brand slider on product

--- iNetEcommerce/iNetEcommerce/ClientApp/src/app/components/user-components/home-user/home-user.component.ts
+++ iNetEcommerce/iNetEcommerce/ClientApp/src/app/components/user-components/home-user/home-user.component.ts
@@ -1,6 +1,5 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Home } from 'src/app/models/home.model';
-import { ImageModel } from 'src/app/models/image.model';
 import { HomeService } from 'src/app/services/home.service';
 declare var $:any;
 
@@ -11,10 +10,11 @@ declare var $:any;
 })
 
 export class HomeUserComponent implements OnInit, AfterViewInit {
-  images: ImageModel[] = [];
+  images: any[] = [];
   homeModel: Home = new Home();
   constructor(
-    private homeService: HomeService
+    private homeService: HomeService,
+    private cd: ChangeDetectorRef
   ) { }
 
   ngAfterViewInit(): void {
@@ -33,31 +33,47 @@ export class HomeUserComponent implements OnInit, AfterViewInit {
           moreText.css("display","inline");
       }
   });
+
+    this.cd.detectChanges();
   }
 
   ngOnInit(): void {
-    this.getHomeSetting();
+
+      this.getHomeSetting();
   }
 
   generateImageCasoul():void {
     if( this.homeModel.banner1 ) {
-      this.images.push( new ImageModel(this.homeModel.banner1));
+      this.images.push({
+        path: this.homeModel.banner1.toString()
+      })
     }
     if( this.homeModel.banner2 ) {
-      this.images.push( new ImageModel(this.homeModel.banner2));
+      this.images.push({
+        path: this.homeModel.banner2.toString()
+      })
     }
     if( this.homeModel.banner3 ) {
-      this.images.push( new ImageModel(this.homeModel.banner3));
+      this.images.push({
+        path: this.homeModel.banner3.toString()
+      })
     }
     if( this.homeModel.banner4 ) {
-      this.images.push( new ImageModel(this.homeModel.banner4));
+      this.images.push({
+        path: this.homeModel.banner4.toString()
+      })
     }
   }
 
   getHomeSetting():void {
     this.homeService.getSetting().subscribe((res)=> {
+
       this.homeModel = res;
+
       this.generateImageCasoul();
+
+      this.cd.detectChanges();
+
     })
   }
 
